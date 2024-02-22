ce29767ac46cb539aff32ff88495792d824e164c
bmagnomax <bmagnomax@gmail.com>
2024-02-22 14:58:11 -0300

    ajuste no login do protheus

--- src/portal_prime/app/Http/Controllers/AuthController.php
+++ src/portal_prime/app/Http/Controllers/AuthController.php
@@ -4,33 +4,70 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Laravel\Sanctum\Sanctum;
 
 class AuthController extends Controller
 {
+
     public function loginProtheus(Request $request)
     {
-        $request->validate([
+        // Decodifica o JSON recebido na requisição
+        $requestData = $request->json()->all();
+
+        // Valida os campos obrigatórios
+        $validator = Validator::make($requestData, [
             'email' => 'required',
             'password' => 'required',
         ]);
 
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // Obtém as credenciais do JSON decodificado
         $credentials = [
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
+            'email' => $requestData['email'],
+            'password' => $requestData['password'],
         ];
 
+        // Tenta autenticar o usuário
         if (Auth::attempt($credentials)) {
+            // Cria um token para o usuário autenticado
             $token = Auth::user()->createToken('token-name')->plainTextToken;
 
+            // Retorna o token como resposta JSON
             return response()->json(['token' => $token]);
         }
 
-        throw ValidationException::withMessages([
-            'codigo' => ['Credenciais inválidas'],
-        ]);
+        // Se as credenciais estiverem incorretas, retorna um erro de validação
+        return response()->json(['error' => 'Credenciais inválidas'], 401);
     }
+
+
+//     public function loginProtheus(Request $request)
+//     {
+//         $request->validate([
+//             'email' => 'required',
+//             'password' => 'required',
+//         ]);
+
+//         $credentials = [
+//             'email' => $request->input('email'),
+//             'password' => $request->input('password'),
+//         ];
+
+//         if (Auth::attempt($credentials)) {
+//             $token = Auth::user()->createToken('token-name')->plainTextToken;
+
+//             return response()->json(['token' => $token]);
+//         }
+
+//         throw ValidationException::withMessages([
+//             'codigo' => ['Credenciais inválidas'],
+//         ]);
+//     }
     public function login(Request $request)
     {
         $request->validate([
